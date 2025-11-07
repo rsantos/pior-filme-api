f5bc7c1a0b85d0c126991da64a96551f80960f71
Rafael Santos <rafael.santos@hox.rs>
2025-11-07 18:28:22 -0300

    test: update producer tests to fetch award intervals

--- src/__tests__/producer.spec.ts
+++ src/__tests__/producer.spec.ts
@@ -1,5 +1,10 @@
+import request from "supertest";
+import app from "../app";
+
+
 describe("Producer", () => {
-  it("should run a sample test", () => {
-    expect(1 + 1).toBe(2);
+  it("should fetch producers with award intervals", async () => {
+    const response = await request(app).get("/producers/award-intervals");
+    expect(response.status).toBe(200);
   });
 });
