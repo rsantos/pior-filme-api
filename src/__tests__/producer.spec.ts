6fb3ea07eb518d6ade878689e9e4fcb983695bf1
Rafael Santos <rafael.santos@hox.rs>
2025-11-07 18:36:11 -0300

    feat: implement award intervals endpoint for producers

--- src/__tests__/producer.spec.ts
+++ src/__tests__/producer.spec.ts
@@ -4,7 +4,34 @@ import app from "../app";
 
 describe("Producer", () => {
   it("should fetch producers with award intervals", async () => {
-    const response = await request(app).get("/producers/award-intervals");
+    const response = await request(app)
+      .get("/producers/award-intervals")
+      .accept("application/json");
+
     expect(response.status).toBe(200);
+    expect(response.body).toEqual({
+      min: [
+        {
+          producer: "Joel Silver",
+          interval: 8,
+          previousWin: 1990,
+          followingWin: 1998,
+        },
+        {
+          producer: "Matthew Vaughn",
+          interval: 8,
+          previousWin: 2002,
+          followingWin: 2010,
+        },
+      ],
+      max: [
+        {
+          producer: "Mark Wahlberg",
+          interval: 13,
+          previousWin: 1997,
+          followingWin: 2010,
+        },
+      ],
+    });
   });
 });
--- src/routes/producer.router.ts
+++ src/routes/producer.router.ts
@@ -5,7 +5,30 @@ const router = Router();
 
 router.get("/award-intervals", (_req, res) => {
   res
-    .send("Lista os produtores que tiveram o maior e o menor intervalo entre prêmios.")
+    .json({
+      min: [
+        {
+          producer: "Joel Silver",
+          interval: 8,
+          previousWin: 1990,
+          followingWin: 1998,
+        },
+        {
+          producer: "Matthew Vaughn",
+          interval: 8,
+          previousWin: 2002,
+          followingWin: 2010,
+        },
+      ],
+      max: [
+        {
+          producer: "Mark Wahlberg",
+          interval: 13,
+          previousWin: 1997,
+          followingWin: 2010,
+        },
+      ],
+    })
     .status(200);
 });
 
